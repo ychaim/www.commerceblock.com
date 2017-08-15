4e55accdc0fb9f82a3d1686db6955599ada1ba75
Omar Shibli <omarshib@gmail.com>
2017-08-15 21:06:02 +0300

    fixed deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws s3 sync --exclude=".git/*" --exclude="scripts" --exclude="*.sh" --exclude=".gitignore" --acl public-read --delete ./ s3://commerceblock.com
+aws s3 sync --exclude=".git/*" --exclude="scripts/*" --exclude="*.sh" --exclude=".gitignore" --acl public-read --delete ./ s3://commerceblock.com
